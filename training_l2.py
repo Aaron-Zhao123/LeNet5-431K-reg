04c715cc9f58a287dcf9ef9ebce69a7d45265b1b
Aaron Zhao <aaron@dhcp-10-249-100-159.eduroam.wireless.private.cam.ac.uk>
2017-02-28 21:24:49 +0000

    lambda

--- training_l2.py
+++ training_l2.py
@@ -336,7 +336,7 @@ def main(argv = None):
         (weights, biases) = initialize_variables(model_number)
         # Construct model
         pred, pool, loss= conv_network(x_image, weights, biases, keep_prob)
-        lambda_2 = 0.0001
+        lambda_2 = 0.0000001
 
         l2_norm = lambda_2 * loss
 
