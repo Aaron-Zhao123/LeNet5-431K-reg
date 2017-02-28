d39c9abcebf66ad413d4bc80c63a1511d23bb3f3
Aaron Zhao <aaron@dhcp-10-248-103-85.eduroam.wireless.private.cam.ac.uk>
2017-02-28 10:14:50 +0000

    train l1 norm

---
+++ run_v6.py
@@ -0,0 +1,60 @@
+import os
+import training_l1
+# os.system('python training_v3.py -p0')
+# os.system('python training_v3.py -p1')
+# os.system('python training_v3.py -p2')
+# os.system('python training_v3.py -p3')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p4')
+# os.system('python training_v3.py -p5')
+
+acc_list = []
+count = 0
+pcov = 0
+pfc = 0
+pcov2 = 0
+pfc2 = 0
+# model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
+# pfc = pfc+1
+# param = [
+# ('-pcov',pcov),
+# ('-pcov2',pcov2),
+# ('-pfc',pfc),
+# ('-pfc2',pfc2),
+# ('-m',model_tag),
+# ('-ponly', True),
+# ('-test', False)
+# ]
+# acc = training_v6.main(param)
+retrain = 0
+lr = 1e-4
+model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
+while (count < 1):
+    pfc = pfc+1
+    if (retrain == 0):
+        lr = 1e-4
+    param = [
+    ('-pcov',pcov),
+    ('-pcov2',pcov2),
+    ('-pfc',pfc),
+    ('-pfc2',pfc2),
+    ('-m',model_tag),
+    ('-lr',lr)
+    ]
+    acc = training_l1.main(param)
+    model_tag = 'pcov'+str(pcov)+'pcov'+str(pcov2)+'pfc'+str(pfc)+'pfc'+str(pfc2)
+    acc_list.append(acc)
+    count = count + 1
+    if (acc < 0.99):
+        retrain += 1
+        lr = lr / float(2)
+        if (retrain > 2):
+            print("lowest precision")
+            break
+    else:
+        if (retrain != 0):
+            retrain = 0
+
+print (acc)
+
+print('accuracy summary: {}'.format(acc_list))
