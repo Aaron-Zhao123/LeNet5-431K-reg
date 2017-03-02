e0c6d167373bbcdd9fadbd48c96a4f6509b435e6
Aaron Zhao <aaron@dhcp-10-249-103-75.eduroam.wireless.private.cam.ac.uk>
2017-03-02 23:36:56 +0000

    hi

--- training_l1.py
+++ training_l1.py
@@ -435,7 +435,7 @@ def main(argv = None):
                                 print('Epoch is {}'.format(epoch))
                                 weights_info(training_cnt, c, train_accuracy, accuracy_mean)
                         # if (training_cnt == 10):
-                        if (accuracy_mean > 0.99 or epoch > 300):
+                        if (accuracy_mean > 0.99 or epoch > 200):
                         # if (epoch > 300):
                             accuracy_list = np.zeros(30)
                             accuracy_mean = 0
@@ -445,7 +445,7 @@ def main(argv = None):
                                     y: mnist.test.labels[:],
                                     keep_prob: 1.})
                             print('test accuracy is {}'.format(test_accuracy))
-                            if (epoch > 300 or test_accuracy > 0.990):
+                            if (epoch > 200 or test_accuracy > 0.990):
                             # if (epoch > 300):
                                 file_name = 'dropout/'+ weight_file_name
                                 with open(file_name, 'wb') as f:
