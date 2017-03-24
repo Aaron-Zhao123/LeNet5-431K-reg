9692acde86b33776561433b5f33b226a1e6a3f05
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-24 17:12:01 +0800

    arg lrs

--- run_v6.py
+++ run_v6.py
@@ -57,6 +57,8 @@ for lnorm1 in lambda1_list:
     l1, l2 = compute_lambda.main(fetch_lambdas_params)
     l1 = 1e-5
     l2 = 1e-3
+    l1 = 0
+    l2 = 0
     print('picked l1 l2 to be {},{}'.format(l1,l2))
     # sys.exit()
     param = [
