c141eda692396170229f839c24d7539a45e69e32
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-24 15:15:01 +0800

    added pkl

--- compute_lambda.py
+++ compute_lambda.py
@@ -192,7 +192,7 @@ def main(argv = None):
 
     correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits = pred, labels = y))
 
     merged = tf.merge_all_summaries()
     saver = tf.train.Saver()
