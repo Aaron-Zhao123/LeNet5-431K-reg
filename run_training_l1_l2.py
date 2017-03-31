eabf467772b76561ba6abbd46d6bb77d35603f97
Aaron Zhao <aaron@user-32-56.vpn.net.private.cam.ac.uk>
2017-03-31 15:22:05 +0800

    log iter counts

--- run_training_l1_l2.py
+++ run_training_l1_l2.py
@@ -20,14 +20,16 @@ def compute_file_name(pcov, pfc):
 
 acc_list = []
 count = 0
-pcov = [0., 0.]
+pcov = [0., 80.]
 pfc = [99.4, 0.]
 
 retrain = 0
 lr = 1e-4
 f_name = compute_file_name(pcov,pfc)
 # pfc[0] = pfc[0] + 0.1
-pcov[1] = pcov[1] + 10
+# pcov[1] = pcov[1] + 10
+pcov[0] = pcov[0] + 10
+pfc[1] = pfc[1] + 10
 
 while (count < 10):
     if (retrain == 0):
@@ -94,7 +96,9 @@ while (count < 10):
                 for item in acc_list:
                     f.write(item)
             # pfc[0] = pfc[0] + 0.1
-            pcov[1] = pcov[1] + 10
+            # pcov[1] = pcov[1] + 10
+            pcov[0] = pcov[0] + 10
+            pfc[1] = pfc[1] + 10
             if (pfc[0] == 100):
                 break
             # pcov[0] = pcov[0] + 10.
@@ -108,7 +112,9 @@ while (count < 10):
             for item in acc_list:
                 f.write(item)
         # pfc[0] = pfc[0] + 0.1
-        pcov[1] = pcov[1] + 10
+        # pcov[1] = pcov[1] + 10
+        pcov[0] = pcov[0] + 10
+        pfc[1] = pfc[1] + 10
         if (pfc[0] == 100):
             break
         # pfc[1] = pfc[1] + 10.
