a79ae91e758a276708ea5ef3229374aab51640f5
Aaron Zhao <aaron@user-35-130.vpn.net.private.cam.ac.uk>
2017-04-03 10:22:15 +0800

    log iter counts

--- run_training_l1_l2.py
+++ run_training_l1_l2.py
@@ -21,12 +21,12 @@ def compute_file_name(pcov, pfc):
 acc_list = []
 count = 0
 pcov = [0., 0.]
-pfc = [90, 0.]
+pfc = [99, 0.]
 
 retrain = 0
 lr = 1e-4
 f_name = compute_file_name(pcov,pfc)
-pfc[0] = pfc[0] + 1
+pfc[0] = pfc[0] + 0.1
 # pcov[1] = pcov[1] + 10
 # pcov[0] = pcov[0] + 10
 # pfc[1] = pfc[1] + 10
@@ -98,7 +98,7 @@ while (count < 10):
             with open("hist.txt","w") as f:
                 for item in acc_list:
                     f.write(item)
-            pfc[0] = pfc[0] + 1
+            pfc[0] = pfc[0] + 0.1
             # pcov[1] = pcov[1] + 10
             # pcov[0] = pcov[0] + 10
             # pfc[1] = pfc[1] + 10
@@ -114,7 +114,7 @@ while (count < 10):
         with open("hist.txt","w") as f:
             for item in acc_list:
                 f.write(item)
-        pfc[0] = pfc[0] + 1
+        pfc[0] = pfc[0] + 0.1
         # pcov[1] = pcov[1] + 10
         # pcov[0] = pcov[0] + 10
         # pfc[1] = pfc[1] + 10
