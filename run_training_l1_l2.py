01da6b1902df8adc2d866ce97edcbd1f67668013
Aaron Zhao <aaron@user-39-81.vpn.net.private.cam.ac.uk>
2017-04-04 22:13:30 +0800

    retrain

--- run_training_l1_l2.py
+++ run_training_l1_l2.py
@@ -21,12 +21,13 @@ def compute_file_name(pcov, pfc):
 acc_list = []
 count = 0
 pcov = [0., 0.]
-pfc = [0., 0.]
+pfc = [90., 0.]
 
 retrain = 0
 lr = 1e-4
 f_name = compute_file_name(pcov,pfc)
-pfc[0] = pfc[0] + 10.
+# pfc[0] = pfc[0] + 10.
+pfc[0] = pfc[0] + 1.
 # pcov[1] = pcov[1] + 10
 # pcov[0] = pcov[0] + 10
 # pfc[1] = pfc[1] + 10
@@ -103,7 +104,8 @@ while (count < 10):
                 for item in acc_list:
                     f.write(item)
             # pcov[1] = pcov[1] + 10
-            pfc[0] = pfc[0] + 10.
+            # pfc[0] = pfc[0] + 10.
+            pfc[0] = pfc[0] + 1.
             # pcov[0] = pcov[0] + 10
             # pfc[1] = pfc[1] + 10
             # pfc[0] = pfc[0] + 0.1
@@ -122,7 +124,8 @@ while (count < 10):
         with open("hist.txt","w") as f:
             for item in acc_list:
                 f.write(item)
-        pfc[0] = pfc[0] + 10.
+        # pfc[0] = pfc[0] + 10.
+        pfc[0] = pfc[0] + 1.
         # pcov[0] = pcov[0] + 10
         # pfc[1] = pfc[1] + 10
         # pfc[0] = pfc[0] + 0.1
